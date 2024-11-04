99f7ada9b5a2403dfb6fd1a1d926668f9668b283
0tk1r <otkirbaykuanishbaev82@gmail.com>
2024-11-04 16:11:37 +0300

    changed path

---
+++ lessons/lesson1.php
@@ -0,0 +1,37 @@
+<?php
+// O'zgeriwshiler ha'm olardin' tipleri (Переменные и их типы данных).
+// O'zgeriwshiler degenimiz ne - O'zgeriwshiler kompyuter yacheykasindag'i qandayda
+// bir kishi yad (память) dep alamiz. Og'an bizler ma'gliwmatlardi kiritemiz ha'r turli
+// stringlar (Строкалар), intlar (числолар), boolean (Логичиские данные), ha'm bul mag'liwmatlardi
+// keyinrek o'zgerte alamiz. Bul o'zgeriwshilerdi keyin ala matematikaliq ameller islewimiz, qatarlardi
+// ekranga shigariw xizmetlerin atqaramiz.
+
+//O'zgeriwshilerdin jaratiliwi to'mendegishe:
+
+// $аты = манисы; 
+// $o'zgeriwshi = tipi;
+
+//O'zgeriwshini ekrang'a shig'ariw 
+
+$number = 10;
+echo $number;
+
+//Eki o'zgeriwshini bir waqitta (bir operator menen) shig'ariw.
+
+$a = 20;
+$b = 2;
+
+echo $a . $b;
+
+// Logikaliq a'meller boolean, bool. 
+// Logikaliq a'meller bolip atirg'an procceste processtin' shin(true, 1) yaki jalg'an(false, 0) ekenligin tekserip beredi.
+
+$bool = false; // jalg'an
+$boolean = true; // shin
+
+// O'zgeriwshilerdin' tipleri:
+ $number = 5; int // Диапазон чисел: // -9,223,372,036,854,775,808 // +9,223,372,036,854,775,807 // 64-bit
+ $num = 4.5; float // Диапазон чисел: // 1,7976931348623157E+308 // 2,22507385072014E-308 // 64-bit
+ $str = "hello" string
+ $bool = false; boolean
+
