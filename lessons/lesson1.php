4632d7fa15c7db06fe5f1217f3fb4750e9e531dd
0tk1r <otkirbaykuanishbaev82@gmail.com>
2024-11-05 23:03:14 +0300

    changed paths

--- lessons/lesson1.php
+++ lessons/lesson1.php
@@ -32,6 +32,26 @@ $boolean = true; // shin
 // O'zgeriwshilerdin' tipleri:
  $number = 5; int // Диапазон чисел: // -9,223,372,036,854,775,808 // +9,223,372,036,854,775,807 // 64-bit
  $num = 4.5; float // Диапазон чисел: // 1,7976931348623157E+308 // 2,22507385072014E-308 // 64-bit
- $str = "hello" string
- $bool = false; boolean
+ $str = "hello" // string 
+ $bool = false; // boolean
 
+// "php" tilinde bizler o'zgeriwshilerge tiplerin bermeymiz. o'zgeriwshiler alg'an ma'nisine qaray 
+//o'zi tipin belgileydi.
+
+//qosimsha funkciyalar:
+//floatval() - bizlerde o'zgeriwshini float tipine alip keledi.
+//intval()  - bizlerde o'zgeriwshini int' tipine alip keledi.
+//boolval() - bizlerde o'zgeriwshini bool tipine alip keledi.
+//strval() - bizlerde o'zgeriwshini string tipine alip keledi.
+
+// Constantalar menen islesiw.
+//Constantalar - bul o'zgeriwshiler siyaqli, biraq constantalarda bir marte ma'nis beremiz
+//keyin ala o'zgerte almaymiz. Al o'zgeriwshilerde  bolsa kerisinshe.
+
+// Constantalardi jaratiwda bizler "define()" funkciyasinnan paydalanamiz.
+// define(name_const, meaning);
+
+//Misali:
+
+ define(MY_AGE, 19);
+ echo MY_AGE . "</br>";
