626ff6ad412191d3bc5d3653c96cc82a856d38e0
0tk1r <otkirbaykuanishbaev82@gmail.com>
2024-11-07 16:21:13 +0300

    changed paths

---
+++ lessons/lesson6.php
@@ -0,0 +1,6 @@
+<?php 
+# Massivlar menen islesiw (Массивы данных. Одномерные и многомерные)
+// Massiv degenimiz ne - Massiv ti bizler o'zgeriwshi dep alsaqta boladi.
+// Biraq o'zgeriwshilerden parqi, o'geriwshilerde bizler tekke 1 ma'nisti bere
+// alamiz, al massivta bolsa bir neshe ma'nislerdi beremiz.
+
